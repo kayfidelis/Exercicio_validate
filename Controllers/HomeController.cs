e82f782399815dfe2312211bef7de44dc403b652
João Fabio Meireles <jmeireles@tecnologiaunica.com.br>
2023-05-02 23:33:51 -0300

    teste

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,68 @@
+﻿using ExeKayqueIV.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ExeKayqueIV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Cliente()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Cliente(Cliente objCliente)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", objCliente);
+            }
+            return View();
+        }
+        public ActionResult Resultado(Cliente ObjCliente)
+        {
+            return View(ObjCliente);
+        }
+
+        public ActionResult SelectLogin(string login)
+        {
+            var ExeKayqueIV = new Collection<string>
+            {
+                "Prático",
+                "Heitor",
+                "Cícero"
+            };
+            return Json(ExeKayqueIV.All(a => a.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet);
+
+        }
+
+        public ActionResult Selectcpf(long CPF)
+        {
+            var ExeKayqueIV = new Collection<long>
+            {
+                11122233311,
+                11122233312,
+                11122233313
+            };
+            return Json(ExeKayqueIV.All(a => a != CPF), JsonRequestBehavior.AllowGet);
+
+        }
+        public ActionResult SelectDate(DateTime DataCadastro)
+        {
+            DateTime hoje = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day, 0, 0, 0);
+            bool validate = hoje <= DataCadastro;
+
+            return Json(validate, JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Web;
+
+namespace ExeKayqueIV.Models
+{
+    public class Cliente
+    {
+        [DisplayName("Código do Cliente")]
+        [Required(ErrorMessage = "O código do clinte é obrigatório")]
+        public int IdCli { get; set; }
+
+        [StringLength(150, MinimumLength = 1, ErrorMessage = "O nome deve conter no maximo 150 caracteres")]
+        [DisplayName("Nome do Clinte ")]
+        [Required(ErrorMessage = "O nome do Clinte é Obrigatorio")]
+        public string NomeCli { get; set; }
+
+        [Required(ErrorMessage = "O CPF é obrigatório")]
+        [DisplayName("CPF")]
+        [System.Web.Mvc.Remote("SelectCPF", "Home", ErrorMessage = "o cpf já existe")]
+        public string CPF { get; set; }
+
+        [StringLength(50, MinimumLength = 3)]
+        [RegularExpression(@"^[a-zA-Z]+(([\'\,\.\-][a-zA-Z ])?[a-zA-Z]*)*\s+<(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})>$|^(\w[-._\w]*\w@\w[-._\w]*\w\.\w{2,3})$", ErrorMessage = "Digite um e-mail válido")]
+        [DisplayName("E-Mail")]
+        [Required(ErrorMessage = "O E-mail é obrigatório")]
+        public string Email { get; set; }
+
+        [StringLength(15, MinimumLength = 1, ErrorMessage = "O nome deve conter no maximo 15 caracteres")]
+        [DisplayName("Login")]
+        [Required(ErrorMessage = "O login é obrigatório")]
+        [System.Web.Mvc.Remote("SelectLogin", "Home", ErrorMessage = "o login já existe")]
+        public string Login { get; set; }
+
+
+        [StringLength(15, MinimumLength = 3)]
+        [Required(ErrorMessage = "A senha é obrigatória")]
+        [DisplayName("Senha")]
+        public string Senha { get; set; }
+
+        [Compare("Senha", ErrorMessage = "As senhas são diferentes")]
+        [DisplayName("Confirme a senha: ")]
+        [Required(ErrorMessage = "A confirmação da senha é obrigatória")]
+        public string ConfirmaSenha { get; set; }
+
+
+        [DataType(DataType.Date)]
+        [System.Web.Mvc.Remote("SelectDate", "Home", ErrorMessage = "A data deve ser igual ou superior a data de hoje")]
+        public string DataCadastro { get; set; }
+    }
+}
